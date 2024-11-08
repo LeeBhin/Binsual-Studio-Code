a34ecc89ae4fc16ddbe0d8284542f3b88efb45c9
LeeBhin <bini4039@naver.com>
2024-11-08 14:31:18 +0900

    activity-bar ui

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,6 +7,12 @@ import {
   VscLayoutSidebarRightOff,
   VscLayoutSidebarLeftOff,
   VscLayout,
+  VscMenu,
+  VscFiles,
+  VscSourceControl,
+  VscExtensions,
+  VscDatabase,
+  VscDebugAlt,
 } from "react-icons/vsc";
 
 const Layout = ({ children }) => {
@@ -16,11 +22,14 @@ const Layout = ({ children }) => {
         <div className={css["titlebar-wrap"]}>
           <div className={css["titlebar-left"]}></div>
           <div className={css["titlebar-center"]}>
-            <div className={css["titlebar-arrow-bg"]}>
+            <div className={css["titlebar-icon-bg"]}>
               <VscArrowLeft className={css["titlebar-arrow"]} />
             </div>
-            <div className={css["titlebar-arrow-bg"]}>
-              <VscArrowRight style={{ color: "rgba(204, 204, 204, 0.5)" }} />
+            <div className={css["titlebar-icon-bg"]}>
+              <VscArrowRight
+                className={css["titlebar-arrow"]}
+                style={{ color: "rgba(204, 204, 204, 0.5)" }}
+              />
             </div>
             <div className={css.search}>
               <div className={css["search-wrap"]}>
@@ -30,14 +39,51 @@ const Layout = ({ children }) => {
             </div>
           </div>
           <div className={css["titlebar-right"]}>
-            <VscLayoutSidebarRightOff />
-            <VscLayoutPanelOff />
-            <VscLayoutSidebarLeftOff />
-            <VscLayout />
+            <div className={css["titlebar-icon-bg"]}>
+              <VscLayoutSidebarLeftOff />
+            </div>
+            <div className={css["titlebar-icon-bg"]}>
+              <VscLayoutPanelOff />
+            </div>
+            <div className={css["titlebar-icon-bg"]}>
+              <VscLayoutSidebarRightOff />
+            </div>
+            <div className={css["titlebar-icon-bg"]}>
+              <VscLayout />
+            </div>
           </div>
         </div>
       </header>
+
+      <header className={css.activitybar}>
+        <div className={css.menubar}>
+          <VscMenu />
+        </div>
+
+        <div className={css.taskbar}>
+          <div className={css["taskbar-wrap"]}>
+            <VscFiles />
+          </div>
+          <div className={css["taskbar-wrap"]}>
+            <VscSearch />
+          </div>
+          <div className={css["taskbar-wrap"]}>
+            <VscSourceControl />
+          </div>
+          <div className={css["taskbar-wrap"]}>
+            <VscDebugAlt />
+          </div>
+          <div className={css["taskbar-wrap"]}>
+            <VscDatabase />
+          </div>
+          <div className={css["taskbar-wrap"]}>
+            <VscExtensions />
+          </div>
+        </div>
+      </header>
+
       <main className={css.main}>{children}</main>
+
       <footer className={css.footer}></footer>
     </div>
   );
