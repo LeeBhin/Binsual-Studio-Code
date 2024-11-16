b033dd11b27d0bbd5e6d4b086ea4c2f1591da0b2
LeeBhin <bini4039@naver.com>
2024-11-16 14:11:33 +0900

    모바일 뷰포트 해결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useEffect } from 'react';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import './styles/App.css';
 
 const App = () => {
+  let vh = 0;
+
+  useEffect(() => {
+    const updateVH = () => {
+      vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    updateVH();
+
+    window.addEventListener('resize', updateVH);
+
+    return () => {
+      window.removeEventListener('resize', updateVH);
+    };
+  }, []);
+
   return (
     <div>
       <Layout>
